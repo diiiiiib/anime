eff3fde3a9065975d3cf08cc780a858edfd4170e
diiiiiib <droomhan@gmail.com>
2026-02-25 22:01:49 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,3 @@
 // Firebase Configuration
 // هذا الملف يحتوي على إعدادات Firebase الخاصة بالموقع
 // يجب ألا يتم رفع هذا الملف إلى مستودع عام
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCGZ4svrHXIlPnINwpw8zNkGr8g634XB00",
-    authDomain: "xodiib.firebaseapp.com",
-    databaseURL: "https://xodiib-default-rtdb.firebaseio.com",
-    projectId: "xodiib",
-    storageBucket: "xodiib.appspot.com",
-    messagingSenderId: "645441544450",
-    appId: "1:645441544450:web:1a17d040d4c21eae1de21b",
-    measurementId: "G-5GC79ZRLTH"
-};
-
-window.firebaseConfig = firebaseConfig;
